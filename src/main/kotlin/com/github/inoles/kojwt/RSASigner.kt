9808670b18002a21bd654eb441f78a7313081d23
Jonathan Steele <iNoles@users.noreply.github.com>
2025-01-21 18:46:03 -0500

    added RSA

---
+++ src/main/kotlin/com/github/inoles/kojwt/RSASigner.kt
@@ -0,0 +1,49 @@
+package com.github.inoles.kojwt
+
+import dev.whyoleg.cryptography.CryptographyAlgorithmId
+import dev.whyoleg.cryptography.CryptographyProvider
+import dev.whyoleg.cryptography.algorithms.Digest
+import dev.whyoleg.cryptography.algorithms.RSA.PKCS1
+import dev.whyoleg.cryptography.algorithms.SHA256
+import dev.whyoleg.cryptography.algorithms.SHA512
+
+enum class RSAAlgorithm(
+    val alg: String,
+    val digest: CryptographyAlgorithmId<Digest>
+) {
+    RS256("SHA256withRSA", SHA256),
+    RS512("SHA512withRSA", SHA512);
+
+    companion object {
+        fun fromString(alg: String) =
+            entries.find { it.name == alg }
+                ?: throw IllegalArgumentException("Unsupported algorithm: $alg")
+    }
+}
+
+class RSASigner(
+    algorithm: RSAAlgorithm,
+    private val privateKey: PKCS1.PrivateKey, // Pass the private key
+    private val publicKey: PKCS1.PublicKey // Pass the public key
+) : JwtSigner {
+    override val alg: String = algorithm.alg
+
+    private val provider = CryptographyProvider.Default // Access the default provider
+    private val hasher = provider.get(algorithm.digest).hasher()
+
+    override suspend fun sign(data: String, secret: String): ByteArray {
+        // Hash the data using the selected digest
+        val hashedData = hasher.hash(data.encodeToByteArray())
+
+        // Generate the signature using the private key
+        return privateKey.signatureGenerator().generateSignature(hashedData)
+    }
+
+    override suspend fun verify(data: String, signature: ByteArray, secret: String): Boolean {
+        // Hash the data using the selected digest
+        val hashedData = hasher.hash(data.encodeToByteArray())
+
+        // Verify the signature using the public key
+        return publicKey.signatureVerifier().tryVerifySignature(hashedData, signature)
+    }
+}
